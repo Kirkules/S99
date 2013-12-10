e6a34c8dab3f4711362c5aa8f65099d67bbd2098
Kirk Boyer <kirk.boyer@gmail.com>
2013-12-10 15:27:34 -0700

    added question22

---
+++ Q22.scala
@@ -0,0 +1,6 @@
+// Kirk Boyer
+// 12/8/2013
+// Scala 99 Questions
+// Question 22: Create a list containing all integers in a given range:
+
+def myRange(lo:Int, hi:Int):List[Int] = List.range(lo,hi+1)
