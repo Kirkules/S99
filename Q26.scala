290f8a9e21c439269e612a1f1064be00fb9c84b6
Kirk Boyer <kirk.boyer@gmail.com>
2013-12-11 19:49:00 -0700

    made Q26 into an object for importing later

--- Q26.scala
+++ Q26.scala
@@ -3,16 +3,17 @@
 // Scala 99 Questions
 // Question 26: Generate the combinations of K distinct objects chosen from a given list.
 
-def KCombinationsFrom[A](ls:List[A], K:Int):List[List[A]] = {
+object Q26{
+  def KCombinationsFrom[A](ls:List[A], K:Int):List[List[A]] = {
 
-  def stickXInFront[B](x:B, xs:List[List[B]]):List[List[B]] = xs.map(x::_)
-  if (ls.length < K) throw new IllegalArgumentException
-  else
-    if (K < 1) throw new IllegalArgumentException
+    def stickXInFront[B](x:B, xs:List[List[B]]):List[List[B]] = xs.map(x::_)
+
+    if (K < 1 || K > ls.length) throw new IllegalArgumentException
     else if (K == 1) ls.map(List(_))
     else if (K == ls.length) List(ls)
-    else (for {i <- List.range(0,ls.length-1)} yield {
+    else (for {i <- List.range(0,ls.length-K+1)} yield {
         stickXInFront(ls(i), KCombinationsFrom(ls.slice(i+1,ls.length), K-1) )
       }).flatten
 
+  }
 }
